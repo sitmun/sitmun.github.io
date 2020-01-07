84713ad4300c373d11e497e7f0e1381ccf03220c
Travis CI User <travis@example.org>
2020-01-07 16:34:29 +0000

    Automatic update of the docs

--- doc-angular/js/menu-wc.js
+++ doc-angular/js/menu-wc.js
@@ -113,13 +113,13 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                                 <a href="modules/SitmunFrontendCoreModule.html" data-type="entity-link">SitmunFrontendCoreModule</a>
                                 <li class="chapter inner">
                                     <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ?
-                                        'data-target="#directives-links-module-SitmunFrontendCoreModule-d95bfc8a05548113a76e164c4b60257d"' : 'data-target="#xs-directives-links-module-SitmunFrontendCoreModule-d95bfc8a05548113a76e164c4b60257d"' }>
+                                        'data-target="#directives-links-module-SitmunFrontendCoreModule-1b48fe119e68104170955a95dd3e2d19"' : 'data-target="#xs-directives-links-module-SitmunFrontendCoreModule-1b48fe119e68104170955a95dd3e2d19"' }>
                                         <span class="icon ion-md-code-working"></span>
                                         <span>Directives</span>
                                         <span class="icon ion-ios-arrow-down"></span>
                                     </div>
-                                    <ul class="links collapse" ${ isNormalMode ? 'id="directives-links-module-SitmunFrontendCoreModule-d95bfc8a05548113a76e164c4b60257d"' :
-                                        'id="xs-directives-links-module-SitmunFrontendCoreModule-d95bfc8a05548113a76e164c4b60257d"' }>
+                                    <ul class="links collapse" ${ isNormalMode ? 'id="directives-links-module-SitmunFrontendCoreModule-1b48fe119e68104170955a95dd3e2d19"' :
+                                        'id="xs-directives-links-module-SitmunFrontendCoreModule-1b48fe119e68104170955a95dd3e2d19"' }>
                                         <li class="link">
                                             <a href="directives/HasAnyAuthorityDirective.html"
                                                 data-type="entity-link" data-context="sub-entity" data-context-id="modules">HasAnyAuthorityDirective</a>
